dd2f7813e984c6e0ca44044127d71f0208acdc06
ILYINGK <ilyin_gk@mail.ru>
2020-12-07 11:12:25 +0300

    begin

---
+++ src/main/java/mail/ilyin_gk/avia/AviaApplication.java
@@ -0,0 +1,13 @@
+package mail.ilyin_gk.avia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mail/ilyin_gk/avia/AviaApplicationTests.java
@@ -0,0 +1,13 @@
+package mail.ilyin_gk.avia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
